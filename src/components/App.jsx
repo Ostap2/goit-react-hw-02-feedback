bb995e693ea012f509cbe31c99724b6b64413a17
Ostap <Ostapkriven13@gmail.com>
2023-09-17 18:05:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './index.css';
+import '../index.css';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
